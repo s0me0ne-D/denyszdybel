5e05e94df43aaca6ba8aa71cafa80ca5b053a1cb
Denys <zdybeldenys@gmail.com>
2024-02-01 20:07:36 +0100

    installed react-device-detect, removed the shadow on title for safari

--- src/parts/title/name/Letter.tsx
+++ src/parts/title/name/Letter.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import { isSafari } from "react-device-detect";
 
 export const Letter = ({ letter, index }: { letter: string; index: number | string }) => {
 	const [stopAnimation, setStopAnimation] = useState(false);
 	return (
 		<span className="letters" onAnimationEnd={() => setStopAnimation(true)}>
 			<span
-				className={`letters_background appears-scale-${index} ${stopAnimation && "stop-animation"}`}
+				className={`letters_background appears-scale-${index} ${
+					stopAnimation && "stop-animation"
+				} ${isSafari && "safari-shadow"}`}
 			>
 				{letter}
 			</span>
