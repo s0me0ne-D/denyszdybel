9817deb7ea85ea5865e838f2430060497ddff703
Denys <zdybeldenys@gmail.com>
2024-02-07 06:22:37 +0100

    changed linkedIn link

--- src/parts/contact/FooterLine.tsx
+++ src/parts/contact/FooterLine.tsx
@@ -12,7 +12,7 @@ export const FooterLine = () => {
 					<GitHubIcon />
 				</a>
 				<a
-					href="https://www.linkedin.com/in/%D0%B4%D0%B5%D0%BD%D0%B8%D1%81-%D0%B7%D0%B4%D0%B8%D0%B1%D0%B5%D0%BB%D1%8C-92443ba5/"
+					href="https://www.linkedin.com/in/denys-zdybel-92443ba5/"
 					target="_blank"
 					rel="noreferrer"
 				>
